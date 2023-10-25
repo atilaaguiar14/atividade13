ba3b27aec2351a74cc970c9f5662d8ea4d799952
atilasousaa285 <atilaaguiarsousa285@gmail.com>
2023-10-25 10:32:01 -0300

    first commit

---
+++ atividade13.py
@@ -0,0 +1,15 @@
+# Exercício Phyton 13: Faça um programa que leia três números e
+# mostre qual é o maior e qual é o menor.
+
+numero1 = int(input("digite o primeiro numero: "))
+numero2 = int(input("digite o segundo numero: "))
+numero3 = int(input("digite o terceiro numero: "))
+
+maior = numero1
+
+if numero1 > numero2 and numero3:
+    print("O numero um é o maior: ", numero1)
+elif numero2 > numero1 and numero3
+    print("O numero dois é o maior: ", numero2)
+elif numero3 > numero1 and numero2
+    print("O numero três é o maior: ", numero3)
